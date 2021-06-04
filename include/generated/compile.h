a6392eaefcaa7ef476d302c52308328b8e54c571
qwe624758472 <624758472@qq.com>
2021-06-04 16:33:41 +0800

    init

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#2 SMP Thu Jun 3 13:48:32 CST 2021"
+#define LINUX_COMPILE_BY "hao"
+#define LINUX_COMPILE_HOST "hao"
+#define LINUX_COMPILER "gcc version 7.4.0 (Debian 7.4.0-6)"
